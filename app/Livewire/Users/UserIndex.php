180c3862fc13abcfe810651c76016525858b862d
MdialloC19 <mdiallo3211@gmail.com>
2023-09-06 17:31:51 +0000

    foutaise

--- app/Livewire/Users/UserIndex.php
+++ app/Livewire/Users/UserIndex.php
@@ -2,13 +2,24 @@
 
 namespace App\Livewire\Users;
 
+use App\Models\User;
 use Livewire\Component;
+
 
 class UserIndex extends Component
 {
+    public $search='';
+
     public function render()
     {
-        return view('livewire.users.user-index');
+        $users=User::all();
+        if(strlen($this->search)>2)
+            {
+                $users=User::where('username', 'like',"%{$this->search}%")->get();
+            }
+        return view('livewire.users.user-index',[
+            'users'=>$users
+        ]);
 
     }
 }
